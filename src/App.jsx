f61f3b4f3a2b6a1afddf4a46a3442e1af5524f9d
Xavi Guasch <xaviguasch@gmail.com>
2023-11-03 17:34:28 +0100

    added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './components/Header'
+
 function App() {
-  return <h1>React Investment Calculator</h1>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App
